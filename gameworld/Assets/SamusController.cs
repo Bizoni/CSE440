5434dc2a9ec80f2967f1729c6aefd180a375663e
Iury <005393228@coyote.csusb.edu>
2015-10-29 21:04:48 -0700

    week 5 game world

---
+++ gameworld/Assets/SamusController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SamusController : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(new Vector3(1, 0, 0) * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(new Vector3(-1, 0, 0) * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(new Vector3(0, 1, 0) * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(new Vector3(0, -1, 0) * moveSpeed * Time.deltaTime);
+    }
+}
