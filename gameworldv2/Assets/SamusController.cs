56fc664523b3371704bb851db90bb43a30d26b64
Iury <005393228@coyote.csusb.edu>
2015-11-05 23:30:34 -0800

    added physics, collisions and triggers

---
+++ gameworldv2/Assets/SamusController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SamusController : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    private bool facingRight = true;
+    Rigidbody2D rb;
+    private bool grounded = true;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        Vector3 theScale = transform.localScale;
+        if (Input.GetKey(KeyCode.RightArrow))
+        { 
+            if (facingRight == false)
+            {
+                theScale.x *= -1;
+                facingRight = true;
+            }
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (facingRight == true)
+            {
+                theScale.x *= -1;
+                facingRight = false;
+            }
+            rb.velocity = new Vector2(moveSpeed * -1 , rb.velocity.y);
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (grounded == true)
+            {
+                rb.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+                grounded = false;
+                Debug.Log("not grounded");
+            }
+        }
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //  transform.Translate(new Vector3(0, -1, 0) * moveSpeed * Time.deltaTime);
+        
+        transform.localScale = theScale;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if( other.CompareTag("cube"))
+        {
+            GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        Debug.Log("grounded");
+        grounded = true;
+    }
+}
