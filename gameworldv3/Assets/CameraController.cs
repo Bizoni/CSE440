beb6f3ba19ea6d30598da2cab222a044f9f9b8af
Iury <005393228@coyote.csusb.edu>
2015-11-12 14:01:30 -0800

    added audio effects, background music and colors counter

---
+++ gameworldv3/Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    //public float rotation;
+    // Use this for initialization
+    void Start () {
+       // rotation = GetComponent<Transform>().parent.rotation.z;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 0, 360-GetComponent<Transform>().parent.rotation.z));
+    }
+}
---
+++ gameworldv3/Assets/ColorsCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ColorsCounter : MonoBehaviour {
+
+    Text counter;
+    public static int colorsNo = 0;
+    // Use this for initialization
+    void Start() {
+        counter = gameObject.GetComponent<Text>();
+        counter.text = "colors: " + colorsNo;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        counter.text = "colors: " + colorsNo;
+    }
+    public static void Countcolor()
+    {
+        colorsNo += 1;
+    }
+}
